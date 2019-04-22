cda06de258816fe700210accb68ab3a3a3d5b2f3
Viviana Quirama <viquiram@uoc.edu>
2019-04-22 17:13:57 +0200

    Add FontAwesome module to the project.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ContactAppComponent } from './contact-app/contact-app.component';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { ContactAppComponent } from './contact-app/contact-app.component';
     ContactAppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
